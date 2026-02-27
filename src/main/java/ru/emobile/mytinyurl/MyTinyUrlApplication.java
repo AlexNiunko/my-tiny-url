acaf366eae54b85645df3a10cf652cffb18f6162
Aliaksandr Niunko <a.niunko@astondevs.ru>
2026-02-27 18:31:22 +0300

    init

---
+++ src/main/java/ru/emobile/mytinyurl/MyTinyUrlApplication.java
@@ -0,0 +1,13 @@
+package ru.emobile.mytinyurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTinyUrlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTinyUrlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/emobile/mytinyurl/MyTinyUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.emobile.mytinyurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTinyUrlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
